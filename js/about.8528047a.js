8cbb04c2aae1fb41e8af03b6ba3f5000426216ba
fedor rodin <rodinfm307@gmail.com>
2022-12-10 09:41:33 +0300

    new prod repo

---
+++ js/about.8528047a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkoleg_tg_webapp_vue"]=self["webpackChunkoleg_tg_webapp_vue"]||[]).push([[443],{9695:function(e,n,u){u.r(n),u.d(n,{default:function(){return p}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(e,n){return(0,t.wg)(),(0,t.iD)("div",a,s)}var o=u(89);const i={},l=(0,o.Z)(i,[["render",c]]);var p=l}}]);
+//# sourceMappingURL=about.8528047a.js.map
